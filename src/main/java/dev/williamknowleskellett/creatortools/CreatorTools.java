f5f424113b73dff98daa07ed5b79e8684c40628a
William Knowles-Kellett <39969985+wknowleskellett@users.noreply.github.com>
2021-12-30 13:21:57 -0500

    Added Item

--- src/main/java/dev/williamknowleskellett/creatortools/CreatorTools.java
+++ src/main/java/dev/williamknowleskellett/creatortools/CreatorTools.java
@@ -1,14 +1,20 @@
 package dev.williamknowleskellett.creatortools;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class ExampleMod implements ModInitializer {
+public class CreatorTools implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("modid");
+	public static final Logger LOGGER = LogManager.getLogger("creatortools");
+	public static final Item ACORN = new Item(new Item.Settings().group(ItemGroup.MISC));
 
 	@Override
 	public void onInitialize() {
@@ -16,6 +22,8 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		// LOGGER.info("Hello Fabric world!");
+
+		Registry.register(Registry.ITEM, new Identifier("creatortools", "acorn"), ACORN);
 	}
 }
--- src/main/java/dev/williamknowleskellett/creatortools/mixin/CreatorToolsMixin.java
+++ src/main/java/dev/williamknowleskellett/creatortools/mixin/CreatorToolsMixin.java
@@ -1,6 +1,6 @@
 package dev.williamknowleskellett.creatortools.mixin;
 
-import dev.williamknowleskellett.creatortools.ExampleMod;
+import dev.williamknowleskellett.creatortools.CreatorTools;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class CreatorToolsMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		CreatorTools.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
